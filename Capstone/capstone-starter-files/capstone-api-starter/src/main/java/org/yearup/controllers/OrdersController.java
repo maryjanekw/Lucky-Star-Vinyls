9204f5969c3fdac0f77ed8a2461cb69f84bc0d9e
Maryjane Watanabe <maryjanekw@gmail.com>
2025-12-19 02:21:49 -0800

    working on OrdersController

--- Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/OrdersController.java
+++ Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,4 +1,29 @@
 package org.yearup.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.OrderDao;
+import org.yearup.data.OrderLineItemDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.data.UserDao;
+
+@RestController
+@RequestMapping("/orders")
 public class OrdersController {
+
+    private final OrderDao orderDao;
+    private final OrderLineItemDao orderLineItemDao;
+    private final ShoppingCartDao shoppingCartDao;
+    private final UserDao userDao;
+
+    public OrdersController(OrderDao orderDao, OrderLineItemDao orderLineItemDao, ShoppingCartDao shoppingCartDao,
+                            UserDao userDao) {
+        this.orderDao = orderDao;
+        this.orderLineItemDao = orderLineItemDao;
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+    }
+
+
 }
