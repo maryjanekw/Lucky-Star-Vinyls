5f895a1cf8486c7f9e69410da8e85618a8a0768a
Maryjane Watanabe <maryjanekw@gmail.com>
2025-12-18 22:12:23 -0800

    working on MySqlShoppingCartDao, removeProduct()

--- Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -74,10 +74,11 @@ public class MySqlShoppingCartDao implements ShoppingCartDao {
     public void clearCart(int userId) {
         String sql = "DELETE FROM shopping_cart WHERE user_id = ?";
         jdbcTemplate.update(sql, userId);
-
     }
 
     @Override
     public void removeProduct(int userId, int productId) {
+        String sql = "DELETE FROM shopping_cart WHERE user        String sql = "DELETE FROM shopping_cart WHERE user_id = ? AND product_id = ?";
+        jdbcTemplate.update(sql, userId, productId);
     }
 }
