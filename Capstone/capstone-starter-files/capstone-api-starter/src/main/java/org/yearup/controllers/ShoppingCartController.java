9b5a75646cda4b8a68f02fd4ff621cd8b0d4d8be
Maryjane Watanabe <maryjanekw@gmail.com>
2025-12-18 16:57:24 -0800

    update ShoppingCartController

--- Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,6 +1,7 @@
 package org.yearup.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
@@ -10,22 +11,27 @@ import org.yearup.models.User;
 
 import java.security.Principal;
 
-// convert this class to a REST controller
-// only logged in users should have access to these actions
+@RestController
+@RequestMapping("/cart")
+@CrossOrigin
+// only loggedin users should have access to these actions
 public class ShoppingCartController
 {
     // a shopping cart requires
-    private ShoppingCartDao shoppingCartDao;
-    private UserDao userDao;
-    private ProductDao productDao;
+    private final ShoppingCartDao shoppingCartDao;
+    private final UserDao userDao;
+    private final ProductDao productDao;
 
-
+    public ShoppingCartController(ShoppingCartDao shoppingCartDao, UserDao userDao, ProductDao productDao) {
+        this.shoppingCartDao = shoppingCartDao;
+        this.userDao = userDao;
+        this.productDao = productDao;
+    }
 
     // each method in this controller requires a Principal object as a parameter
-    public ShoppingCart getCart(Principal principal)
-    {
-        try
-        {
+    @GetMapping
+    public ShoppingCart getCart(Principal principal) {
+        try {
             // get the currently logged in username
             String userName = principal.getName();
             // find database user by userId
@@ -33,22 +39,32 @@ public class ShoppingCartController
             int userId = user.getId();
 
             // use the shoppingcartDao to get all items in the cart and return the cart
-            return null;
-        }
-        catch(Exception e)
-        {
+            return shoppingCartDao.getByUserId(userId);
+        } catch(Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad. 8");
         }
     }
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
+    @PostMapping("/product/{productId}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addProduct(@PathVariable int productId, Principal principal) {
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            shoppingCartDao.addProduct(userId, productId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not add product.");
+        }
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be updated)
     // the BODY should be a ShoppingCartItem - quantity is the only value that will be updated
-
 
     // add a DELETE method to clear all products from the current users cart
     // https://localhost:8080/cart
--- Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -5,5 +5,7 @@ import org.yearup.models.ShoppingCart;
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
+
+    void addProduct(int userId, int productId);
     // add additional method signatures here
 }
