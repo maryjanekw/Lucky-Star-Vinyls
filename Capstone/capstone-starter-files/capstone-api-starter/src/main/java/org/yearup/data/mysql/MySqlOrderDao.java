a48fa89ca145c2f2872371bd3403d6cd7e3e3c4d
Maryjane Watanabe <maryjanekw@gmail.com>
2025-12-19 01:48:47 -0800

    working on MySqlOrderDao, createOder()

--- Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
+++ Capstone/capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -1,11 +1,39 @@
 package org.yearup.data.mysql;
 
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.yearup.data.OrderDao;
+import org.yearup.models.Order;
 
 @Repository
 public class MySqlOrderDao implements OrderDao {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public MySqlOrderDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        String sql = """
+            INSERT INTO orders (user_id, date, address, city, state, zip, shipping_amount)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """;
+
+        jdbcTemplate.update(sql,
+                order.getUserId(),
+                java.sql.Timestamp.valueOf(order.getDate()),
+                order.getAddress(),
+                order.getCity(),
+                order.getState(),
+                order.getZip(),
+                order.getShippingAmount());
+
+        Integer orderId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Integer.class);
+        order.setOrderId(orderId);
+        return order;
+    }
 
 }
